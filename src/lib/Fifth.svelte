50c141582d993454814f22a0a28d13592cd20924
aMediocreDad <amediocredad@outlook.com>
2022-11-05 23:10:10 +0100

    wip: Blech

---
+++ src/lib/Fifth.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Map from "./Map.svelte";
+	export let API_KEY;
+
+	let ready = true;
+</script>
+
+<svelte:head>
+	<script defer async src="https://maps.googleapis.com/maps/api/js?key={API_KEY}&callback=initMap&v=weekly">
+	</script>
+</svelte:head>
+
+{#if ready}
+	<Map />
+{/if}
+
+<style>
+	:global(body) {
+		padding: 0;
+	}
+</style>
